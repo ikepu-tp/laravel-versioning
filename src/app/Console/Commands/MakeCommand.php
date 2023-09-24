c44ce371136e9c7e2a3d3f3a7892193d484005a9
ikepu-tp <postmaster@licsss.jp>
2023-09-24 16:00:08 +0900

    add: version

--- src/app/Console/Commands/MakeCommand.php
+++ src/app/Console/Commands/MakeCommand.php
@@ -2,6 +2,7 @@
 
 namespace ikepu_tp\LaravelVersioning\app\Console\Commands;
 
+use Error;
 use Illuminate\Console\Command;
 
 class MakeCommand extends Command
@@ -12,7 +13,7 @@ class MakeCommand extends Command
      * @var string
      */
     protected $signature = 'versioning:make
-                            {--V|version= : version type (major, minor, patch)}
+                            {--VT|version_type= : version type (major, minor, patch)}
                             {--J|major : version type is major}
                             {--M|minor : version type is minor}
                             {--P|patch : version type is patch}
@@ -25,15 +26,19 @@ class MakeCommand extends Command
      */
     protected $description = 'Generate release note.';
 
+    protected $versions;
     /**
      * Execute the console command.
      */
     public function handle()
     {
+        dump($this->getVersions(), $this->generateReleaseNote());
     }
 
     protected function getVersions()
     {
+        if ($this->versions) return $this->versions;
+
         $path = base_path("version.json");
         if (!file_exists($path)) {
             $this->error("Could not find `version.json` file.");
@@ -41,14 +46,14 @@ class MakeCommand extends Command
         }
 
         $file = file_get_contents($path);
-        $versions = json_decode($file, true);
-        return $versions;
+        $this->versions = json_decode($file, true);
+        return $this->versions;
     }
 
     protected function generateReleaseNote()
     {
         $newVersion = [
-            "version" => "",
+            "version" => $this->generateVersion($this->getVersionType()),
             "releaseDate" => "",
             "Author" => "",
             "url" => "",
@@ -61,5 +66,52 @@ class MakeCommand extends Command
             "futurePlans" => "",
             "note" => "",
         ];
+
+        return $newVersion;
+    }
+
+    protected function getVersionType(): string|null
+    {
+        $version_type = $this->option("version_type");
+        if ($this->option("patch")) $version_type = "patch";
+        if ($this->option("minor")) $version_type = "minor";
+        if ($this->option("major")) $version_type = "major";
+        if (is_null($version_type)) {
+            foreach (["major", "minor", "patch"] as $vt) {
+                if (!$this->confirm("Do you want to increase `{$vt}`?")) continue;
+                $version_type = $vt;
+                break;
+            }
+        }
+        return $version_type;
+    }
+
+    protected function generateVersion(string $version_type = null): string|false
+    {
+        if (!in_array($version_type, ["major", "minor", "patch"]))
+            return $this->ask("What's next version?", "0.0.0");
+        $versions = $this->getVersions();
+        $prev_version = count($versions) ? $versions[count($versions) - 1] : ["version" => "0.0.0"];
+        $splited_prev_version = explode('.', preg_replace("/[^0-9\.]/", "", $prev_version["version"]));
+        switch ($version_type) {
+            case 'major':
+                $splited_prev_version[0] = (int)$splited_prev_version[0] + 1;
+                break;
+            case 'minor':
+                if ($splited_prev_version[1]) {
+                    $splited_prev_version[1] = (int)$splited_prev_version[1] + 1;
+                } else {
+                    $splited_prev_version[1] = 1;
+                }
+                break;
+            case 'patch':
+                if ($splited_prev_version[2]) {
+                    $splited_prev_version[2] = (int)$splited_prev_version[2] + 1;
+                } else {
+                    $splited_prev_version[2] = 1;
+                }
+                break;
+        }
+        return implode(".", $splited_prev_version);
     }
 }
