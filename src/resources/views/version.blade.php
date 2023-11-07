8458d1a2be6949549f71f4ea319b65162dc6e90a
ikepu-tp <postmaster@licsss.jp>
2023-11-07 13:17:38 +0900

    fix

--- src/resources/views/version.blade.php
+++ src/resources/views/version.blade.php
@@ -3,54 +3,62 @@
 ])
 @extends('LaravelVersioning::layout')
 @section('content')
-  <div class="accordion">
+  <div class="accordion" id="versions">
     @foreach ($versions as $version)
-      <div class="according-item">
-        <h2 class="according-header">v{{ $version['version'] }}</h2>
-        <div class="accordiong-body">
-          <div>
-            <span>作成日：{{ $version['createdDate'] }}</span>
-            <span>リリース日：{{ $version['releaseDate'] }}</span>
-          </div>
-          <div>
+      <div class="accordion-item">
+        <h2 class="accordion-header">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+            data-bs-target="#v{{ $version['version'] }}-body" aria-expanded="false"
+            aria-controls="v{{ $version['version'] }}-body">
+            v{{ $version['version'] }}
+          </button>
+        </h2>
+        <div id="v{{ $version['version'] }}-body" class="accordion-collapse collapse" data-bs-parent="#versions">
+          <div class="accordion-body">
             <div>
-              リリース寄与者
-              <ul>
-                @foreach ($version['Author'] as $author)
-                  <li>{{ $author }}</li>
-                @endforeach
-              </ul>
+              <span>作成日：{{ $version['createdDate'] }}</span>
+              <span>リリース日：{{ $version['releaseDate'] }}</span>
             </div>
-            @if (!empty($version['url']))
+            <div>
               <div>
-                参考URL
+                リリース寄与者
                 <ul>
-                  @foreach ($version['url'] as $url)
-                    <li>
-                      <a href="{{ $url }}" target="_blank">{{ $url }}</a>
-                    </li>
+                  @foreach ($version['Author'] as $author)
+                    <li>{{ $author }}</li>
                   @endforeach
                 </ul>
               </div>
-            @endif
-          </div>
-          <div>
-            <h3>説明</h3>
-            {{ $version['description'] }}
-            <h3>新機能</h3>
-            {{ $version['newFeatures'] }}
-            <h3>変更機能</h3>
-            {{ $version['changedFeatures'] }}
-            <h3>削除機能</h3>
-            {{ $version['deletedFeatures'] }}
-            <h3>注意事項</h3>
-            {{ $version['notice'] }}
-            <h3>セキュリティ</h3>
-            {{ $version['security'] }}
-            <h3>今後の予定</h3>
-            {{ $version['futurePlans'] }}
-            <h3>お知らせ</h3>
-            {{ $version['note'] }}
+              @if (!empty($version['url']))
+                <div>
+                  参考URL
+                  <ul>
+                    @foreach ($version['url'] as $url)
+                      <li>
+                        <a href="{{ $url }}" target="_blank">{{ $url }}</a>
+                      </li>
+                    @endforeach
+                  </ul>
+                </div>
+              @endif
+            </div>
+            <div>
+              <h3>説明</h3>
+              {{ $version['description'] }}
+              <h3>新機能</h3>
+              {{ $version['newFeatures'] }}
+              <h3>変更機能</h3>
+              {{ $version['changedFeatures'] }}
+              <h3>削除機能</h3>
+              {{ $version['deletedFeatures'] }}
+              <h3>注意事項</h3>
+              {{ $version['notice'] }}
+              <h3>セキュリティ</h3>
+              {{ $version['security'] }}
+              <h3>今後の予定</h3>
+              {{ $version['futurePlans'] }}
+              <h3>お知らせ</h3>
+              {{ $version['note'] }}
+            </div>
           </div>
         </div>
       </div>
