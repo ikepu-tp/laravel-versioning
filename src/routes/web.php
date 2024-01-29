9ebef02fcc9d564bd814a6c1c52046417058ff77
ikepu-tp <postmaster@licsss.jp>
2024-01-30 06:50:11 +0900

    add middleware

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,7 +3,13 @@
 use ikepu_tp\LaravelVersioning\app\Http\Controllers\VersionController;
 use Illuminate\Support\Facades\Route;
 
-Route::group([], function () {
+Route::group([
+    "middleware" => config("versioning.web_middleware", [])
+], function () {
     Route::get("version", [VersionController::class, "index"])->name("version.index");
     Route::get("version/{version}", [VersionController::class, "show"])->name("version.show");
+});
+Route::group([
+    "middleware" => config("versioning.api_middleware", [])
+], function () {
 });
