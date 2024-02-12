804fdb86d5e895ba17b3054d7915d63345916b7a
ikepu-tp <postmaster@licsss.jp>
2024-02-13 07:24:28 +0900

    update

--- src/config/versioning.php
+++ src/config/versioning.php
@@ -3,6 +3,32 @@
 return [
     /**
      * ----------------------------------------------------------------
+     * Web Route Middleware
+     * ----------------------------------------------------------------
+     */
+    "web_middleware" => [
+        "web",
+    ],
+    "editor_web_middleware" => [
+        "web",
+        \ikepu_tp\LaravelVersioning\app\Http\Middleware\VersioningEditorMiddleware::class,
+    ],
+
+    /**
+     * ----------------------------------------------------------------
+     * API Route Middleware
+     * ----------------------------------------------------------------
+     */
+    "api_middleware" => [
+        "api",
+    ],
+    "editor_api_middleware" => [
+        "api",
+        \ikepu_tp\LaravelVersioning\app\Http\Middleware\VersioningEditorMiddleware::class,
+    ],
+
+    /**
+     * ----------------------------------------------------------------
      * Except keys
      * ----------------------------------------------------------------
      */
@@ -27,4 +53,11 @@ return [
      */
     "list_view" => "version-list",
     //"list_view"=>"version-with-detail",
+
+    /**
+     * ----------------------------------------------------------------
+     * Prefix on Editor Pages
+     * ----------------------------------------------------------------
+     */
+    "editor_prefix" => "versioning",
 ];
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,9 +1,27 @@
 <?php
 
+use ikepu_tp\LaravelVersioning\app\Http\Controllers\EditorController;
 use ikepu_tp\LaravelVersioning\app\Http\Controllers\VersionController;
 use Illuminate\Support\Facades\Route;
 
-Route::group([], function () {
+Route::group([
+    "middleware" => config("versioning.web_middleware", []),
+], function () {
     Route::get("version", [VersionController::class, "index"])->name("version.index");
     Route::get("version/{version}", [VersionController::class, "show"])->name("version.show");
+});
+Route::group([
+    "middleware" => config("versioning.editor_web_middleware", []),
+    "prefix" => config("versioning.editor_prefix", "versioning"),
+], function () {
+    Route::resource("editor", EditorController::class)->names("version.editor");
+});
+Route::group([
+    "middleware" => config("versioning.api_middleware", [])
+], function () {
+});
+Route::group([
+    "middleware" => config("versioning.editor_api_middleware", []),
+    "prefix" => config("versioning.editor_prefix", "versioning"),
+], function () {
 });
